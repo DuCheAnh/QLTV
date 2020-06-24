ccee30146b1313ad157fc6d6be699b9e161a565c
DuCheAnh <ducheanh1308@gmail.com>
2020-06-24 15:21:24 +0700

    added Borrow and Book dataclasses

---
+++ DTO_QuanLy/Book_Data.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QuanLy
+{
+    public class Book_Data
+    {
+        public string BID;
+        public string name;
+        public string author;
+        public DateTime release_date;
+        public string category;
+        public string description;
+        public string cover_page;
+        public int price;
+        public int amount;
+        public string added_by;
+        public string add_date;
+        public Book_Data()
+        {
+        }
+        public Book_Data(string sBID, string sName, string sAuthor, DateTime dReleaseDate, string sCategory
+                         , string sDescription, string sCover_Page, int nPrice, int nAmount, string sAdded_By, string sAdd_Date )
+        {
+            this.BID = sBID;
+            this.name = sName;
+            this.author = sAuthor;
+            this.release_date = dReleaseDate;
+            this.category = sCategory;
+            this.description = sDescription;
+            this.cover_page = sCover_Page;
+            this.price = nPrice;
+            this.amount = nAmount;
+            this.added_by = sAdded_By;
+            this.add_date = sAdd_Date;
+        }
+
+    }
+}
---
+++ DTO_QuanLy/Borrow_Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QuanLy
+{
+    public class Borrow_Data
+    {
+        public string BrID;
+        public string BID;
+        public DateTime borrow_date;
+        public DateTime return_date;
+        public int overdue_by;
+        public bool returned;
+        public Borrow_Data()
+        {
+        }
+        public Borrow_Data(string sBrID, string sBID, DateTime dBorrowDate, DateTime dReturnDate, int nOverdueBy, bool bReturned )
+        {
+            this.BrID = sBrID;
+            this.BID = sBID;
+            this.borrow_date = dBorrowDate;
+            this.return_date = dReturnDate;
+            this.overdue_by = nOverdueBy;
+            this.returned = bReturned;
+        }
+    }
+}
