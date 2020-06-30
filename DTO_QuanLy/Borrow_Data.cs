807a3e1a914c21c090f82357c3c8fd415f2969ad
DuCheAnh <ducheanh1308@gmail.com>
2020-06-30 23:11:02 +0700

    added more variables to dataclasses

--- DTO_QuanLy/Borrow_Data.cs
+++ DTO_QuanLy/Borrow_Data.cs
@@ -8,12 +8,13 @@ namespace DTO_QuanLy
 {
     public class Borrow_Data
     {
-        public string BrID;
-        public string BID;
-        public DateTime borrow_date;
-        public DateTime return_date;
-        public int overdue_by;
-        public bool returned;
+        public string BrID { get; set; }
+        public string BID { get; set; }
+        public string UID { get; set; }
+        public DateTime borrow_date { get; set; }
+        public DateTime return_date { get; set; }
+        public int overdue_by { get; set; }
+        public bool returned { get; set; }
         public Borrow_Data()
         {
         }
