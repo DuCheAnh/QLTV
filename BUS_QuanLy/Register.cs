be0da7ef4380a06506bb7377524499d6bd3ddb06
DuCheAnh <ducheanh1308@gmail.com>
2020-07-07 22:53:17 +0700

    change func's name

--- BUS_QuanLy/Register.cs
+++ BUS_QuanLy/Register.cs
@@ -14,7 +14,7 @@ namespace BUS_QuanLy
         public bool RegisterIn(string Account,string Password,string Email)
         {
             DAL_Get.init_client();
-            return DAL_Get.Create_new_user(Account, Password, Email);
+            return DAL_Get.create_new_user(Account, Password, Email);
         }
 
     }
