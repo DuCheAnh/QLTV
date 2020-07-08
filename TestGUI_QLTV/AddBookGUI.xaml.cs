6a7b4bb4713922f477aa8f23abde2446797a2def
unknown <18520358@gm.uit.edu.vn>
2020-07-08 14:52:49 +0700

    dataUI

---
+++ TestGUI_QLTV/AddBookGUI.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestGUI_QLTV
+{
+    /// <summary>
+    /// Interaction logic for AddBookGUI.xaml
+    /// </summary>
+    public partial class AddBookGUI : Window
+    {
+        public AddBookGUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
