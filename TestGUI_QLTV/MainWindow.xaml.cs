6ba37a6d38fef8f492c97a266cdb97e2510c435d
TB3C-Hieu <55200386+TB3C-Hieu@users.noreply.github.com>
2020-07-20 20:53:22 +0700

    Dang xuat

--- TestGUI_QLTV/MainWindow.xaml.cs
+++ TestGUI_QLTV/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace TestGUI_QLTV
     public partial class MainWindow : Window
     {
         User_Control_BUS Bus_method = new User_Control_BUS();
-
+        bool IsLogout = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -21,6 +21,7 @@ namespace TestGUI_QLTV
             spMenu.Children.Add(mMenu);
             MainPage mPage = new MainPage();
             spMain.Children.Add(mPage);
+            
         }
 
 
@@ -71,12 +72,37 @@ namespace TestGUI_QLTV
 
         private void btnLogout_Click(object sender, RoutedEventArgs e)
         {
+            MessageBoxResult result = MessageBox.Show("Đăng xuất ?", "Question", MessageBoxButton.OKCancel);
+            switch (result)
+            {
+                case MessageBoxResult.OK:
+                User_Control_BUS control_BUS = new User_Control_BUS();
+            Data_Context.currentAccount = null;
+            Data_Context.currentUID = null;
+            Data_Context.currentHomePageBook = null;
+            IsLogout = true;
+            Window1 Login = new Window1();
+            Window.GetWindow(this).Close();
+            Login.Show();
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
 
         }
+        }
+        
+        
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Environment.Exit(Environment.ExitCode);
+            if (IsLogout == true)
+            {
+                IsLogout = false;
+            }
+            else
+            {
+                Environment.Exit(Environment.ExitCode);
+            }
         }
     }
 }
